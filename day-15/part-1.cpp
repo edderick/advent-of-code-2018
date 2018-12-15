465c35278cc5b95a6298efd5648dded23982893e
Edward Seabrook <edderick@live.co.uk>
2018-12-15 23:27:50 +0000

    Part-1 clean up

--- day-15/part-1.cpp
+++ day-15/part-1.cpp
@@ -9,26 +9,20 @@
 
 using namespace std;
 
-void printGrid(const vector<string>& grid)
-{
-    for (const string& row : grid)
-    {
-        for (const char c : row)
-        {
+namespace {
+void printGrid(const vector<string>& grid) {
+    for (const string& row : grid) {
+        for (const char c : row) {
             cout << c << " ";
         }
         cout << "\n";
     }
 }
 
-void printDistanceGrid(const vector<vector<int>>& distances)
-{
-    for (const auto& row : distances)
-    {
-        for (const auto& cell : row)
-        {
-            if (cell == INT_MAX)
-            {
+void printDistanceGrid(const vector<vector<int>>& distances) {
+    for (const auto& row : distances) {
+        for (const auto& cell : row) {
+            if (cell == INT_MAX) {
                 cout << ". ";
             }
             else {
@@ -38,6 +32,9 @@ void printDistanceGrid(const vector<vector<int>>& distances)
         cout << "\n";
     }
 }
+} // Anonymous namespace
+
+struct EndGame {};
 
 class Unit {
   public:
@@ -62,16 +59,11 @@ class Unit {
     
     friend ostream& operator<<(ostream& out, const Unit& unit); 
 
-    vector<vector<int>> calculateDistances(const vector<string>& grid) const
-    {
-        // Hi, I'm not sure if this is going to be efficient enough, but it's
-        // probably close enough!
+    vector<vector<int>> calculateDistances(const vector<string>& grid) const {
         vector<vector<int>> distances;
-        for (const auto& row : grid)
-        {
+        for (const auto& row : grid) {
             vector<int> r; 
-            for (const auto& cell : row)
-            {
+            for (const auto& cell : row) {
                 r.push_back(INT_MAX);
             }
             distances.push_back(r);
@@ -84,19 +76,16 @@ class Unit {
 
         set<pair<int, int>> visited;
 
-        while (!q.empty())
-        {
+        while (!q.empty()) {
             const auto& x = q.front();
             int dist = x.first;
             const auto coord = x.second;
             q.pop_front();
             if (coord.first < 0 || coord.first >= distances[0].size() || 
-                coord.second < 0 || coord.second >= distances.size())
-            {
+                coord.second < 0 || coord.second >= distances.size()) {
                 continue; 
             }
-            if (visited.count(coord) != 0)
-            {
+            if (visited.count(coord) != 0) {
                continue; 
             }
             visited.insert(coord);
@@ -104,12 +93,10 @@ class Unit {
                  (d_type == Unit::GOBLIN && grid[coord.second][coord.first] == 'G') ||
                  (d_type == Unit::ELF && grid[coord.second][coord.first] == 'E')
                 ) && 
-                (d_x != coord.first || d_y != coord.second))
-            {
+                (d_x != coord.first || d_y != coord.second)) {
                 continue;
             }
-            if (dist < distances[coord.second][coord.first])
-            {
+            if (dist < distances[coord.second][coord.first]) {
                 distances[coord.second][coord.first] = dist;
             }
 
@@ -121,8 +108,7 @@ class Unit {
         return distances;
     }
 
-    pair<int, int> calculateNextStep(const vector<vector<int>>& distances, const Unit& target)
-    {
+    pair<int, int> calculateNextStep(const vector<vector<int>>& distances, const Unit& target) {
         pair<int,int> t(target.d_x, target.d_y);
 
         while (distances[t.second][t.first] != 1) {
@@ -143,79 +129,59 @@ class Unit {
         return t;
     }
 
-    void attack(vector<string>& grid, const vector<vector<int>>& distances, vector<Unit>& units)
-    {
+    void attack(vector<string>& grid, const vector<vector<int>>& distances, vector<Unit>& units) {
         int minHp = INT_MAX;
         Unit *target = NULL;
-        for (auto& unit : units)
-        {
-            if (d_type == unit.d_type)
-            {
+        for (auto& unit : units) {
+            if (d_type == unit.d_type) {
                 continue;
             }
 
-            if (distances[unit.d_y][unit.d_x] == 1)
-            {
-                if (unit.d_hitPoints > 0 && unit.d_hitPoints < minHp)
-                {
+            if (distances[unit.d_y][unit.d_x] == 1) {
+                if (unit.d_hitPoints > 0 && unit.d_hitPoints < minHp) {
                     target = &unit;
                     minHp = unit.d_hitPoints;
                 }
-                else if (unit.d_hitPoints > 0 && unit.d_hitPoints == minHp)
-                {
-                    if (unit < *target)
-                    {
+                else if (unit.d_hitPoints > 0 && unit.d_hitPoints == minHp) {
+                    if (unit < *target) {
                         target = &unit;
                         minHp = unit.d_hitPoints;
                     }
                 }
             }
         }
-        if (target)
-        {
-            //cout << *this << " attacked: " << *target << "\n";
+        if (target) {
             target->d_hitPoints -= d_attackPower;
-            if (target->d_hitPoints <= 0)
-            {
+            if (target->d_hitPoints <= 0) {
                 grid[target->d_y][target->d_x] = '.';
             }
         }
     }
 
-    void takeTurn(vector<string>& grid, vector<Unit>& units)
-    {
-        // each unit that is still alive takes a turn
+    void takeTurn(vector<string>& grid, vector<Unit>& units) {
         if (d_hitPoints <= 0)
         {
             throw runtime_error("I am dead.");
         }
 
-        // Find target
         vector<vector<int>> distances = calculateDistances(grid); 
-        //printDistanceGrid(distances);
 
         int minDistance = INT_MAX;
         Unit *targetUnit = NULL;
 
         bool hasTarget = false; 
-        for (auto& unit : units)
-        {
-            if (unit.d_type != d_type)
-            {
-                if (unit.d_hitPoints <= 0)
-                {
+        for (auto& unit : units) {
+            if (unit.d_type != d_type) {
+                if (unit.d_hitPoints <= 0) {
                     continue;
                 }
                 hasTarget = true;
-                if (distances[unit.d_y][unit.d_x] < minDistance)
-                {
+                if (distances[unit.d_y][unit.d_x] < minDistance) {
                     minDistance = distances[unit.d_y][unit.d_x];
                     targetUnit = &unit;
                 }
-                else if (distances[unit.d_y][unit.d_x] == minDistance)
-                {
-                    if (targetUnit && unit < *targetUnit)
-                    {
+                else if (distances[unit.d_y][unit.d_x] == minDistance) {
+                    if (targetUnit && unit < *targetUnit) {
                         minDistance = distances[unit.d_y][unit.d_x];
                         targetUnit = &unit;
                     }
@@ -224,24 +190,20 @@ class Unit {
             }
         }
         
-        if (!hasTarget)
-        {
-            throw 1;
+        if (!hasTarget) {
+            // Yes, I am using exxceptions for control flow. It came to this...
+            EndGame end; 
+            throw end;
         }
 
-        if (!targetUnit)
-        {
-            return; // No-one to target :(
+        if (!targetUnit) {
+            return;
         }
-        //cout << *this << " is targeting: " << *targetUnit << "\n";
 
-        if (minDistance == 1) // Can attack
-        {
-            //cout << "ATTACK!\n";  
+        if (minDistance == 1) {
             attack(grid, distances, units);
         } 
-        else 
-        {
+        else {
             const auto p = calculateNextStep(distances, *targetUnit);
 
             grid[p.second][p.first] = grid[d_y][d_x];
@@ -249,35 +211,25 @@ class Unit {
             d_y = p.second;
             d_x = p.first;
 
-            if (minDistance == 2)
-            {
-                //cout << "ATTACK! (after moved)\n"; 
-                //TODO: Optimize
+            if (minDistance == 2) {
                 vector<vector<int>> distances = calculateDistances(grid); 
                 attack(grid, distances, units);
             }
         }
-
     }
 
-    bool operator<(const Unit& other) const
-    {
-        if (d_y != other.d_y)
-        {
+    bool operator<(const Unit& other) const {
+        if (d_y != other.d_y) {
             return d_y < other.d_y;
         }
-        else 
-        {
+        else {
             return d_x < other.d_x;
         }
     }
-
 };
 
-ostream& operator<<(ostream& out, const Unit& unit) 
-{
-    switch (unit.d_type)
-    {   
+ostream& operator<<(ostream& out, const Unit& unit) {
+    switch (unit.d_type) {   
         case Unit::ELF:
             out << "E";
             break;
@@ -289,29 +241,24 @@ ostream& operator<<(ostream& out, const Unit& unit)
     return out;
 }
 
+int main(int argc, char* argv[]) {
+    const bool VERBOSE = false;
 
-int main(int argc, char* argv[])
-{
     vector<string> grid;
     string line;
-    while (getline(cin, line))
-    {
+    while (getline(cin, line)) {
         grid.push_back(line);
     }
 
     vector<Unit> units;
     int y = 0;
-    for (const string& row : grid)
-    {
+    for (const string& row : grid) {
         int x = 0; 
-        for (const auto& cell : row)
-        {
-            if (cell == 'E')
-            {
+        for (const auto& cell : row) {
+            if (cell == 'E') {
                 units.emplace_back(x, y, Unit::ELF);
             }
-            else if (cell == 'G')
-            {
+            else if (cell == 'G') {
                 units.emplace_back(x, y, Unit::GOBLIN);
             }
             x++;
@@ -321,98 +268,56 @@ int main(int argc, char* argv[])
 
     sort(units.begin(), units.end());
 
-    for (auto& unit : units)
-    {
-        //cout << unit << "\n";
+    if (VERBOSE) {
+        cout << "0:\n";
+        for (auto& unit : units) {
+            cout << unit << "\n";
+        }
+        printGrid(grid);
     }
-    //printGrid(grid);
 
-    for (int i = 1; i < 300; i++)
-    {
+    for (int i = 1; i < 300; i++) {
         sort(units.begin(), units.end());
 
-        for (auto& unit : units)
-        {
-            bool hasElf = false;
-            bool hasGoblin = false;
+        for (auto& unit : units) {
             int sum = 0; 
-            for (auto& unit : units)
-            {
-                if (unit.d_hitPoints <= 0)
-                {
-                    continue;
-                }
-                sum += unit.d_hitPoints;
-                if (unit.d_type == Unit::GOBLIN)
-                {
-                    hasGoblin = true;
-                }
-                else if (unit.d_type == Unit::ELF)
-                {
-                    hasElf = true;
+            for (auto& unit : units) {
+                if (unit.d_hitPoints > 0) {
+                    sum += unit.d_hitPoints;
                 }
             }
-            //if (!hasElf || !hasGoblin)
-            //{
-            //    printGrid(grid);
-            //    cout << "STOP! " << (i - 1) << " * " << sum  << " = " <<  sum * (i - 1) << "\n";
-            //    return 0;
-            //}
 
-            if (unit.d_hitPoints > 0)
-            {
+            if (unit.d_hitPoints > 0) {
                 try {
                     unit.takeTurn(grid, units);
-                } catch(...) {
+                } 
+                catch(EndGame&) {
                     cout << "STOP! " << (i - 1) << " * " << sum  << " = " <<  sum * (i - 1) << "\n";
                     return 0;
                 }
             }
         }
-        for (auto& unit : units)
-        {
-            //cout << unit << "\n";
+
+        if (VERBOSE) {
+            cout << i << ":\n";
+            for (auto& unit : units) {
+                if (unit.d_hitPoints > 0) {
+                    cout << unit << "\n";
+                }
+            }
+            printGrid(grid);
         }
+
         vector<Unit> remainers;
-        for (auto& unit : units)
-        {
-            if (unit.d_hitPoints <= 0)
-            {
+        for (auto& unit : units) {
+            if (unit.d_hitPoints <= 0) {
                 continue;
             }
             remainers.push_back(unit);
         }
         
         sort(remainers.begin(), remainers.end());
-
-#if 0
-        bool hasElf = false;
-        bool hasGoblin = false;
-        int sum = 0; 
-        cout << i << ":\n";
-        for (auto& unit : remainers)
-        {
-            //cout << unit << "\n";
-            sum += unit.d_hitPoints;
-            if (unit.d_type == Unit::GOBLIN)
-            {
-                hasGoblin = true;
-            }
-            else if (unit.d_type == Unit::ELF)
-            {
-                hasElf = true;
-            }
-        }
-        printGrid(grid);
-        if (!hasElf || !hasGoblin)
-        {
-            cout << "DONE! " << i << " * " << sum  << " = " <<  sum * i << "\n";
-            return 0;
-        }
-        cout << "\n";
-#endif
         units = remainers;
-
     }
 
     return 0;
